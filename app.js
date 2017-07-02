c5c59ad4ed18c348b269a4cc44b885f5ab5bf621
johnrooksby <rooksby@googlemail.com>
2017-07-02 20:49:42 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+
+var publication = "hello mum 2017"
+var HTMLpub1 = "<div class=\"row\"><div class=\"col-md-2 col-md-offset-0\"><p><span class=\"label label-info\">workshop paper</span></p></div>";
+var HTMLpub2 = "<div class=\"col-md-9 col-md-offset-0\"><p>" + publication + "</p></div></div>";
+
+
+$(function() {
+  $(".publications").append("<p>YO</p>");
+});
