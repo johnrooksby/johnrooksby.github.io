100e7e4a02ed7ea611e027529d68b06ac84fe0ce
johnrooksby <rooksby@googlemail.com>
2017-07-02 21:10:44 +0100

    Create basic functionality of app.js

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 
-var publication = "hello mum 2017"
-var HTMLpub1 = "<div class=\"row\"><div class=\"col-md-2 col-md-offset-0\"><p><span class=\"label label-info\">workshop paper</span></p></div>";
-var HTMLpub2 = "<div class=\"col-md-9 col-md-offset-0\"><p>" + publication + "</p></div></div>";
+var publication = [
+					
+					{"title": "hello mum 2017", "author": "John R"},
+					{"title": "hi again 2018", "author" : "John and Jim"}
+
+]
 
 
 $(function() {
-  $(".publications").append("<p>YO</p>");
+	var HTMLpub1 = "<div class=\"row\"><div class=\"col-md-2 col-md-offset-0\"><p><span class=\"label label-info\">workshop paper</span></p></div>";
+	var HTMLpub2 = "<div class=\"col-md-9 col-md-offset-0\"><p>";
+	var HTMLpub3 = "</p></div></div>";
+
+	$( publication ).each( function (index, item) {
+ 				$(".publications").append(HTMLpub1 + HTMLpub2 + item.title + item.author + HTMLpub3);
+			}
+		)
+
 });
