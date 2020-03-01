c8b90ba02459e65f67186bfa2975e6bee6d09821
johnrooksby <rooksby@googlemail.com>
2020-03-01 22:36:24 +0000

    make a better website for 2020

---
+++ precache-manifest.90e7cc76dd5dab63d4ec7bb117edea8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15d680ac3dcd65b3167631480740254",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7104538743c36dc1cba",
+    "url": "/static/css/main.9fd55e3e.chunk.css"
+  },
+  {
+    "revision": "adda126d84506461992a",
+    "url": "/static/js/2.21396804.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.21396804.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7104538743c36dc1cba",
+    "url": "/static/js/main.ea876635.chunk.js"
+  },
+  {
+    "revision": "0ae51d22362a6e272248",
+    "url": "/static/js/runtime-main.ab35496a.js"
+  }
+]);
